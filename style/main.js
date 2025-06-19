22f8187d571e9cc2aa1d044e3a0b315a9d6c90c5
EnriYllanes <Enri.Yllanes@gmail.com>
2025-06-18 21:40:30 -0300

    primerrepositoriojavascript

---
+++ style/main.js
@@ -0,0 +1,31 @@
+const cartBtn = document.getElementById('cart-btn')
+const cartPanel = document.getElementById('cart-panel')
+let dejoAbiertoElCarrito =
+    JSON.parse(localStorage.getItem("carrito_open")) || false
+
+
+if (dejoAbiertoElCarrito) {
+    cartPanel.classList.add('active')
+} else {
+    cartPanel.classList.remove('active')
+}
+
+
+
+cartBtn.addEventListener('click', (PerritoConChaucha) => {
+    localStorage.setItem('carrito_open', JSON.stringify(dejoAbiertoElCarrito))
+})
+if (dejoAbiertoElCarrito) {
+    cartPanel.classList.add('active')
+} else {
+    cartPanel.classList.remove('active')
+}
+
+
+function renderizarCards() {
+    products.forEach(el => {
+        let producto = '< div class="product-card"><img src="img/2-2.jpg" alt="reme marron oscura"> /><h3>Day After Day</h3><p>Remera Oversize - Algodon</p><span class="price">$23.000</span><button id="add-to-cart-btn" class="add-to-cart">Agregar al Carrito</button></div>'
+    })
+}
+
+renderizarCards()
---
+++ style/products.js
@@ -0,0 +1,11 @@
+const products = [
+    {
+        id: 'enrique',
+        title: 'juguete de Peluche',
+        description: 'Suave Peluche con chirriador para horas de diversion.',
+        price: 17000,
+        Image: 'https://placedog.net/400/300?id=1',
+        alt: 'Juguete de Peluche para Perrito'
+    },
+    {}
+];
